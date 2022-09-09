de7581098ea7431cce43bd113f5c76d23a5031fe
Elena <vodopyan.elena@gmail.com>
2022-09-09 13:43:05 +0300

    Example_2

--- Example_2/Program.cs
+++ Example_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите трехзначние число ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 100) System.Console.WriteLine("Нет третьей цифры");
+else
+{
+    int result = (x % 100) % 10;
+    System.Console.WriteLine($"Третья цифра = {result}");
+}
