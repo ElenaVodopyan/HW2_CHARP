264d3288c3f6f49e89d57595a489280b6f3f7f9e
Elena <vodopyan.elena@gmail.com>
2022-09-09 11:36:47 +0300

    Example_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+System.Console.WriteLine("Введите трехзначние число ");
+int x = Convert.ToInt32(Console.ReadLine());
+int result = ( x / 10 ) %  10;
+System.Console.WriteLine($"Вторая цифра = {result}");
